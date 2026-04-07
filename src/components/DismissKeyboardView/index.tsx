442b558498127ce22d72c55bf8d4c85dfb0002ce
Matheus Muruci <matheusmuruci@outlook.com>
2026-04-07 16:05:39 -0300

    Adicionado componentes de: Botao, Input, DismissKeyboardView e AuthHeader

---
+++ src/components/DismissKeyboardView/index.tsx
@@ -0,0 +1,15 @@
+import { FC, PropsWithChildren } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Keyboard, KeyboardAvoidingView, ScrollView, TouchableWithoutFeedback } from "react-native";
+
+export const DismissKeyboardView: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <SafeAreaView className="flex-1 bg-background-primary">
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <KeyboardAvoidingView behavior="padding" className="flex-1">
+          <ScrollView>{children}</ScrollView>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
+  );
+};
