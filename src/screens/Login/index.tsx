cdd34e1e9b28a1bbd52e322d43d891bf3e3ed718
Matheus Muruci <matheusmuruci@outlook.com>
2026-04-07 16:09:58 -0300

    Montada a funcionalidade da tela de Login

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,18 +1,22 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 
 import { PublicStackParamsList } from "@/routes/PublicRoutes";
+import { DismissKeyboardView } from "@/components/DismissKeyboardView";
+
+import { LoginForm } from "./LoginForm";
+import { AuthHeader } from "@/components/AuthHeader";
 
 export const Login = () => {
   const navigation = useNavigation<StackNavigationProp<PublicStackParamsList>>();
 
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Login teste</Text>
-      <TouchableOpacity onPress={() => navigation.navigate("Register")}>
-        <Text>Registrar</Text>
-      </TouchableOpacity>
-    </View>
+    <DismissKeyboardView>
+      <View className="flex-1 w-[82%] self-center">
+        <AuthHeader />
+        <LoginForm />
+      </View>
+    </DismissKeyboardView>
   );
 };
